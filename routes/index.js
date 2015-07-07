0ec175999d7a4940acc3becc5092964d531e54ba
Trey Huffine <treyhuffine@gmail.com>
2015-07-07 16:22:50 -0700

    Serve fake data

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
+var data = require('../tweets.json');
+console.log(data);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-app.user(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 router.get('/search', function(req, res, next) {
-
+  res.json(data);
 });
 
 module.exports = router;
