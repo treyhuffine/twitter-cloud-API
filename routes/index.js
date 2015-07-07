07219bbab27364d4f3b9296d761954850ac61f8e
Trey Huffine <treyhuffine@gmail.com>
2015-07-07 16:45:38 -0700

    Prepare to hit real Twitter API

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 var data = require('../tweets.json');
-console.log(data);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/search', function(req, res, next) {
+router.post('/search', function(req, res, next) {
+  console.log(req.body);
   res.json(data);
 });
 
