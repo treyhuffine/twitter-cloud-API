8d6f2e61db4d0d9848e5acdc1560c0832a548011
Trey Huffine <treyhuffine@gmail.com>
2015-07-07 16:55:37 -0700

    Add Twitter API calls

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var client = new Twitter({
+  consumer_key: process.env.CK,
+  consumer_secret: process.env.CS,
+  access_token_key: process.env.ATK,
+  access_token_secret: process.env.ATS
+});
+
+
 router.post('/search', function(req, res, next) {
-  console.log(req.body);
-  res.json(data);
+  var words = req.body.words.split(" ");
+
+  client.get('search/tweets', { q: words.join(" OR "), count: 100 }, function(error, tweets, response) {
+    if(!error)
+    res.json(data);
+  });
 });
 
 module.exports = router;
