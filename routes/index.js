f67d3c1f041a0da3b96383c77b3c616f7c0c33f2
Trey Huffine <treyhuffine@gmail.com>
2015-07-08 11:29:29 -0700

    Handle edge case and increase efficiency

--- routes/index.js
+++ routes/index.js
@@ -20,19 +20,23 @@ var client = new Twitter({
 router.post('/search', function(req, res, next) {
   var words = req.body.words.split(" ");
 
-  client.get('search/tweets', { q: words.join(" OR "), count: 100 }, function(error, tweets, response) {
-    if(!error) {
-      var stats = {}, tweetText;
+  client.get('search/tweets', { q: words.join(" OR "), count: 100 }, function(error, tweets, response){
+    if (!error) {
+      var stats = {}, tweetText = "";
+
       tweets.statuses.forEach(function(tweet) {
-        tweetText = tweet.text.toLowerCase();
-        words.forEach(function(word) {
-          stats[word] = stats[word] || 0;
-          if (tweetText.match(word.toLowerCase())) {
-            stats[word]++;
-          }
-        });
+        tweetText += " " + tweet.text.toLowerCase();
       });
-      console.log(stats);
+
+      words.forEach(function(word) {
+        stats[word] = stats[word] || 0;
+
+        var r = new RegExp(word, 'gi');
+        var matches = tweetText.match(r);
+        if (matches) {
+          stats[word] = matches.length;
+        }
+      });
       res.json(stats);
     }
   });
